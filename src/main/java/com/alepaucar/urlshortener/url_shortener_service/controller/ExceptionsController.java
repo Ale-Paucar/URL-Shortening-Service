c28ff39692a7078ed5c8dbcb8c5410ab0f7fc45b
Ale-Paucar <nuala.ni.ale@gmail.com>
2025-11-11 13:26:35 -0500

    Added url validation

--- src/main/java/com/alepaucar/urlshortener/url_shortener_service/controller/ExceptionsController.java
+++ src/main/java/com/alepaucar/urlshortener/url_shortener_service/controller/ExceptionsController.java
@@ -3,14 +3,24 @@ package com.alepaucar.urlshortener.url_shortener_service.controller;
 import com.alepaucar.urlshortener.url_shortener_service.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionsController {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> notFoundExpectionsController (NotFoundException e ){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    public ResponseEntity<String> notFoundExpectionsController (NotFoundException e ){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> validationExpectionsController (MethodArgumentNotValidException e ){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
 }
--- src/main/java/com/alepaucar/urlshortener/url_shortener_service/dto/OriginalURLRequestDTO.java
+++ src/main/java/com/alepaucar/urlshortener/url_shortener_service/dto/OriginalURLRequestDTO.java
@@ -1,14 +1,18 @@
 package com.alepaucar.urlshortener.url_shortener_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OriginalURLRequestDTO {
+    @NotBlank(message = "The URL no be empty")
+    @URL(message = "The URL isn't valid")
     private String url;
 }
